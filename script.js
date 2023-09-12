d771bd6a56ee65cf4599e7d07e4fb77cd300d088
r3workz <122603849+r3workz@users.noreply.github.com>
2023-09-12 19:32:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+let team = players
+let team1 = players.map(i=>i)
+
+let cap1 = person.map(per=>per)
+
+
 window.players = players;
 window.person = person;
 window.team = team;
